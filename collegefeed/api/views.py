ff764c81f10f1b60b74800778a44bb4ba33d541b
icancode23 <ernipunarora@gmail.com>
2017-02-05 19:44:25 +0530

    completed the results api

--- collegefeed/api/views.py
+++ collegefeed/api/views.py
@@ -6,16 +6,39 @@ import json
 # Create your views here.
   
 ################# API FUNCTION FOR RESULTS ######
-def results(request):
+# A dictionary for result category used akin to switch case in this case
+result_counterdict={
+	'1':mphilcounters,
+	'2':undergradcounters,
+	'3':postgradcounters,
+	'4':diplomacounters,
+	'5':ncwebcounters
+}
+result_resultsdict={
+	'1':mphilresults,
+	'2':undergradresults,
+	'3':postgradresults,
+	'4':diplomaresults,
+	'5':ncwebresults
+}
+def results(request,category):
+	print category
+	global result_counterdict
+	category_counter=result_counterdict[category]
+	global result_resultsdict
+	category_result=result_resultsdict[category]
+	print category_counter,category_result
 	start_index=request.GET.get("from")
 	end_index=request.GET.get("to")
-	current_indexindb=mphilcounters.objects.all()[0].mphilid - 6
+	current_indexindb=category_counter.objects.all()[0].mphilid - 6
 	index_range=int(end_index)-int(start_index)
-	queryset=mphilresults.objects.filter(id__range=(current_indexindb-int(end_index),current_indexindb-int(start_index))).values()
+	queryset=category_result.objects.filter(id__range=(current_indexindb-int(end_index),current_indexindb-int(start_index))).values()
 	responseDict={}
 	for i in range(len(queryset)):
 		responseDict[i+1]=queryset[i];
 
 	jsonResponse=json.dumps(responseDict,indent=4)
 	return HttpResponse(jsonResponse,content_type="application/json")
+
+##################### END OF RESULT API #################
 
