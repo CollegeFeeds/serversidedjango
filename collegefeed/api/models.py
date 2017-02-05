6eee605e2082da76e961ee341e3268d73d9abcd3
icancode23 <ernipunarora@gmail.com>
2017-02-05 12:11:52 +0530

    added an auto increment field

--- collegefeed/api/models.py
+++ collegefeed/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 # A model for Mphil results
 class mphilresults(models.Model):
-	id=models.IntegerField(primary_key=True)
+	id=models.AutoField(primary_key=True)
 	title=models.TextField(unique=False)
 	linkf=models.TextField(unique=False)
 	def __unicode__(self):
