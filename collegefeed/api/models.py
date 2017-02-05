8e64b1b8c87988306da66f76feee233e8ffb5f92
icancode23 <ernipunarora@gmail.com>
2017-02-05 16:40:05 +0530

    added all result models

--- collegefeed/api/models.py
+++ collegefeed/api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-# A model for Mphil results
+################### RESULTS ########################################
+#Tables for M.phil Results-----------------------------
 class mphilcounters(models.Model):
 	mphilid=models.IntegerField(primary_key=True)
 	mphiltitle=models.TextField(unique=False)
@@ -14,5 +15,53 @@ class mphilresults(models.Model):
 	linkf=models.TextField(unique=False)
 	def __unicode__(self):
 	 	return self.title
+#---------------------------------------------------------
 
+#Tables for Ncweb Results-----------------------------
+class ncwebcounters(models.Model):
+	ncwebid=models.IntegerField(primary_key=True)
+	ncwebtitle=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.mphiltitle
+
+
+class ncwebresults(models.Model):
+	title=models.TextField(unique=False)
+	linkf=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.title
+
+#---------------------------------------------------
+
+#Tables for UndergradResults-----------------------------
+class undergradcounters(models.Model):
+	undergradid=models.IntegerField(primary_key=True)
+	udergradtitle=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.mphiltitle
+
+
+class undergradresults(models.Model):
+	title=models.TextField(unique=False)
+	linkf=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.title
+
+#-----------------------------------------------
+
+#Tables for postgradresults-----------------------------
+class postgradcounters(models.Model):
+	postgradid=models.IntegerField(primary_key=True)
+	postgradtitle=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.mphiltitle
+
+
+class postgradresults(models.Model):
+	title=models.TextField(unique=False)
+	linkf=models.TextField(unique=False)
+	def __unicode__(self):
+	 	return self.title
+#--------------------------------------------------------
+##################### END OF RESULT MODELS####################
 
